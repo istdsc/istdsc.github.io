cfce750b613e1d37a764b121818bda198709d0ef
istdsc <50015701+istdsc@users.noreply.github.com>
2019-04-26 06:17:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 <script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
 <script type='text/javascript'>    
-  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1B_5y3Ma1HvR82xkkBZ9Vwn2Aui0h3fKkG2NqrXYl8AE/pubhtml';
+  var publicSpreadsheetUrl = ‘https://docs.google.com/spreadsheets/d/1B_5y3Ma1HvR82xkkBZ9Vwn2Aui0h3fKkG2NqrXYl8AE/pubhtml';
 
   function init() {
     Tabletop.init( { key: publicSpreadsheetUrl,
