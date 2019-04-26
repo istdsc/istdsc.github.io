aae365ec16842eb3ad6cc6d6767320d25ff3a3d5
istdsc <50015701+istdsc@users.noreply.github.com>
2019-04-26 06:02:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function init() {
- Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
+ Tabletop.init( { key: ‘1B_5y3Ma1HvR82xkkBZ9Vwn2Aui0h3fKkG2NqrXYl8AE',
  callback: function(data, tabletop) { 
  console.log(data)
  },
