1ef13fd9ccd45cef29ba179f934b44d300e5d959
istdsc <50015701+istdsc@users.noreply.github.com>
2019-04-26 06:03:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function init() {
- Tabletop.init( { key: ‘1B_5y3Ma1HvR82xkkBZ9Vwn2Aui0h3fKkG2NqrXYl8AE',
+ Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1B_5y3Ma1HvR82xkkBZ9Vwn2Aui0h3fKkG2NqrXYl8AE/pubhtml',
  callback: function(data, tabletop) { 
  console.log(data)
  },
